4b70e4a7a9ca34bfe4ec49a382386fafd33d3a69
emiliendurif <emilien.durif@gmail.com>
2019-07-25 00:06:01 +0200

    maj entete cours et TP

--- make.py
+++ make.py
@@ -14,30 +14,33 @@ f=open('log.txt','w')
 path=r"progression_2019_2020_IPT_MPSI.xlsx"#chemin de la progression
 path_classe=''
 path_site='site'#Chemin pour exporter les pdf vers site
-nligne=40#Derniere ligne où figure une donnée
-delta_td=1#position du jour des TD dans la semaine
-delta_cours=1#position du jour des cours dans la semaine
-delta_tp=3#position du jour des TD dans la semaine
+
 
 ####
 #Definition des colonnes dans le tableau excel
 ####
-col_date,col_num_cycle,col_name_cycle,col_num_cours,col_num_tp,col_name_cours,col_name_tp,col_supports_TD,col_supports_tp,col_competences_cours,col_competences_tp,col_figures,col_ref_cours\
-=1,2,3,4,8,5,9,10,11,12,13,16,17
-
-#Ouverture de la progression excel
-classeur=xlrd.open_workbook(path)
-feuilles=classeur.sheet_names()
-
-#Ouverture de la feuille du semanier
-d0=datetime.date(1900,1,1)
-for f in feuilles:
-    if "Semanier" in f:
-        fs=classeur.sheet_by_name(f)
 
 
-def lire_semanier(fs):
+
+
+
+def lire_semanier(path):
     """a partir d'une feuille excel correspondant au sémanier de la progression renvoie deux listes de tuple donnant des infos sur les cours et td : (date,cycle,numero,nom du cycle,nom du document, supports d'application pour le cours)"""
+    #Ouverture de la progression excel
+    classeur=xlrd.open_workbook(path)
+    feuilles=classeur.sheet_names()
+    
+    #Ouverture de la feuille du semanier
+    d0=datetime.date(1900,1,1)
+    for f in feuilles:
+        if "Semanier" in f:
+            fs=classeur.sheet_by_name(f)
+    col_date,col_num_cycle,col_name_cycle,col_num_cours,col_num_tp,col_name_cours,col_name_tp,col_supports_TD,col_supports_tp,col_competences_cours,col_competences_tp,col_figures,col_ref_cours\
+    =1,2,3,4,8,5,9,10,11,12,13,16,17
+    nligne=40#Derniere ligne où figure une donnée
+    delta_td=1#position du jour des TD dans la semaine
+    delta_cours=1#position du jour des cours dans la semaine
+    delta_tp=3#position du jour des TD dans la semaine
     liste_cours=[]
     liste_cycle=[]
     liste_tp=[]
@@ -88,10 +91,6 @@ def lire_semanier(fs):
                 info_tp.append((date_tp,n_cycle,num_tp,name_cycle,name_tp,supports,competences,figures,ref_cours))
     return info_tp,info_cours
             
-(info_tp,info_cours)=lire_semanier(fs)
-
-#A partir des infos de cours ou TP, écrire dans l'entête les infos correspondantes dans le chemin spécifié nommé rep
-(date,n_cycle,num_activite,name_cycle,name_activite,supports,competences,figures,ref_cours)=info_cours[0]
 
 
     
@@ -117,7 +116,7 @@ def genere_fichiers_tex(info_activite,type_activite):
         os.system('cp style/Cy_i_Ch_j_Cours_PDF.tex '+rep+'/cours/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_activite)+'_Cours_PDF.tex')
         changer_ligne(rep+'/cours/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_activite)+'_Cours_PDF.tex','\\input{Cy_01_Ch_01_Cours.tex}','\\input{Cy_0'+str(n_cycle)+'_Ch_0'+str(num_activite)+'_Cours.tex}')
     elif type_activite=='tp':
-        num_chapitre=ref_cours.split('-')[1]
+        num_chapitre=ref_cours.split(';')[0].split('-')[1]
         if os.path.exists(rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite)==False:
             os.mkdir(rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite)
         os.system('cp style/Cy_i_Ch_j_TP_k.tex '+rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite+'.tex')
@@ -160,15 +159,45 @@ def changer_ligne(fichier,ancienne_ligne,nouvelle_ligne):
 #                     texte_entete+=ligne
 #         f.write(texte_entete)
         
-        
+
+def trouver_chapitre(ref_cours):
+    '''A partir de la reference du cours, trouve le ou les numero de chapitre et renvoie dans num_chapitre'''
+    num_chapitre=ref_cours.split(';')
+    if len(num_chapitre)<=1:
+        num_chapitre=ref_cours.split('-')[1]
+    else:
+        num_chap=''
+        for chap in num_chapitre:
+            num_chap+=chap.split('-')[1]+' et '
+        num_chapitre=num_chap[:-4]
+    return num_chapitre
+    
+def trouve_exo_source(support):
+    '''A partir d'un support renvoie le nom de l'exo et sa source'''
+    classeur=xlrd.open_workbook('inventaire_exos.xlsx')
+    feuilles=classeur.sheet_names()
+    col_domaine,col_fichier,col_exo,col_sources=0,1,2,3
+    exo,source='',''
+    for f in feuilles:
+        if "exos" in f:
+            fs=classeur.sheet_by_name(f)
+            k=1
+            while fs.cell_value(k,0)!='fin':
+                if fs.cell_value(k,col_domaine)+'/'+fs.cell_value(k,col_fichier)==support:
+                    exo=fs.cell_value(k,col_exo)
+                    source=fs.cell_value(k,col_sources)
+                k+=1
+    return exo,source
+
 def genere_entete(rep,info_activite,type_activite):
     """A partir du cours dont le chemin est donne par rep la fonction genere l'entete donnant toutes infos permettant de generer l'entete du cours."""
     (date,n_cycle,num_activite,name_cycle,name_activite,supports,competences,figures,ref_cours)=info_activite
+    #competences=competences.split(';')
+    num_chapitre=trouver_chapitre(ref_cours)
     if type_activite=='cours':
         file_entete=rep+'/cours/info_entete.tex'
     elif type_activite=='tp':
-        num_chapitre=ref_cours.split('-')[1]
-        file_entete=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite+'/info_entete.tex'
+        file_entete=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre[0])+'_TP_'+num_activite+'/info_entete.tex'
     else:
         print("mauvais choix d'activite")
     with open(file_entete,'w',encoding='utf-8') as f:
@@ -184,18 +213,96 @@ def genere_entete(rep,info_activite,type_activite):
                 elif '\\def\\xxnomchapitre' in ligne:
                     texte_entete+='\\def\\xxnomchapitre{'+name_activite+'}\n'
                 elif '\\def\\xxnumchapitre' in ligne:
-                    texte_entete+='\\def\\xxnumchapitre{'+str(num_activite)+'}\n'
+                    texte_entete+='\\def\\xxnumchapitre{'+str(num_chapitre)+'}\n'
+                elif '\\def\\xxnumactivite' in ligne:
+                    texte_entete+='\\def\\xxnumactivite{'+str(num_activite)+'}\n'
                 elif '\\def\\xxchapitre' in ligne:
-                    texte_entete+='\\def\\xxchapitre{'+str(num_activite)+'}\n'
+                    texte_entete+='\\def\\xxchapitre{'+str(num_chapitre)+'}\n'
                 elif '\\def\\xxdate' in ligne:
                     texte_entete+='\\def\\xxdate{'+date+'}\n'
                 elif '\\chapterimage{' in ligne:
                     texte_entete+='\\chapterimage{'+figures+'}\n'
+                elif '\\graphicspath{{../../../style/png/}{images/}' in ligne:
+                    texte_entete+='\\graphicspath{{../../../style/png/}{images/}'
+                    for support in supports.split(';'):
+                        texte_entete+='{../../../exos/'+support+'/}'
+                    texte_entete+='}\n'
                 elif '\\begin{itemize}[label=\\ding{112},font=\\color{ocre}]' in ligne:
-                    texte_entete+='\\begin{itemize}[label=\\ding{112},font=\\color{ocre}]\n\\item '+competences+'\n'
+                    texte_entete+='\\begin{itemize}[label=\\ding{112},font=\\color{ocre}]\n'
+                    code_competence,nom_long,nom_court=trouver_texte_competence(competences,path)
+                    print(code_competence)
+                    for k in range(len(code_competence)):
+                        texte_entete+='\\item '+code_competence[k]+' '+nom_court[k]+'\n'
+                elif '%Infos sur les supports' in ligne:
+                    texte_entete+='%Infos sur les supports\n'
+                    for support in supports.split(';'):
+                        exo,source=trouve_exo_source(support)
+                        texte_entete+='\\def\\xxtitreexo{'+exo+'}\n'
+                        texte_entete+='\\def\\xxsourceexo{\\hspace{.2cm} \\footnotesize{'+source+'}}\n'
                 else: 
                     texte_entete+=ligne
         f.write(texte_entete)
+
+def trouver_texte_competence(competences,path):
+    '''A partir d'une liste de competence renvoie la competence detaillee'''
+    classeur=xlrd.open_workbook(path)
+    feuilles=classeur.sheet_names()
+    col_code,col_nom_long,col_nom_court=0,1,2
+    for f in feuilles:
+        if "Competences" in f:
+            fs=classeur.sheet_by_name(f)
+            k=1
+            nom_court=[]
+            nom_long=[]
+            code_competence=[]
+            while fs.cell_value(k,0)!='fin':
+                if fs.cell_value(k,col_code) in competences:
+                    nom_court.append(fs.cell_value(k,col_nom_court))
+                    nom_long.append(fs.cell_value(k,col_nom_long))
+                    code_competence.append(fs.cell_value(k,col_code))
+                k+=1
+    return code_competence,nom_long,nom_court
+    
+        
+def genere_support(rep,info_activite,type_activite):
+    '''A partir d'une activite genere le fichier support'''
+    (date,n_cycle,num_activite,name_cycle,name_activite,supports,competences,figures,ref_cours)=info_activite
+    rep=trouver_repertoire(info_activite) 
+    num_chapitre=trouver_chapitre(ref_cours)
+    if type_activite=='cours':
+        rep_activite=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre[0])+'_TD_'+num_activite
+    elif type_activite=='tp':
+        rep_activite=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre[0])+'_TP_'+num_activite
+    with open(rep_activite+'/tp.tex','w',encoding='utf-8') as f:
+        if len(supports)>0:
+            for support in supports.split(';'):
+                f.write('\\input{'+'../../../exos/'+support+'}\n')
+        else:
+            f.write('\n')
+    return None
+
+# type_activite='tp'
+# info_activite=info_tp[0]
+# (date,n_cycle,num_activite,name_cycle,name_activite,supports,competences,figures,ref_cours)=info_activite
+# rep=trouver_repertoire(info_activite) 
+# num_chapitre=trouver_chapitre(ref_cours)
+# if type_activite=='cours':
+#     rep_activite=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TD_'+num_activite
+# elif type_activite=='tp':
+#     rep_activite=rep+'/Cy_0'+str(n_cycle)+'_Ch_0'+str(num_chapitre)+'_TP_'+num_activite
+# with open(rep_activite+'/tp.tex','w',encoding='utf-8') as f:
+#     for support in supports.split(';'):
+#         f.write('\\input{'+'../../../exos/'+support+'}\n')
+# #     
+
+
+
+  
+  #######
+#Programme Principal
+#######
+
+(info_tp,info_cours)=lire_semanier(path)
 
 for cours in info_cours:
     (date,n_cycle,num_activite,name_cycle,name_activite,supports,competences,figures,ref_cours)=cours
@@ -208,6 +315,8 @@ for tp in info_tp:
     rep=trouver_repertoire(tp)
     genere_fichiers_tex(tp,'tp')
     genere_entete(rep,tp,'tp')
+    genere_support(rep,tp,'tp')
+    
 
         
         
