cd7765de20dfebaccd3f699b80ba9b1b2dc3d3db
emiliendurif <emilien.durif@gmail.com>
2019-11-25 23:17:26 +0100

    modif D02S

---
+++ exos/tableaux/percolation/percolation.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from numpy.random import rand
+from matplotlib.colors import ListedColormap
+
+def creation_grille(p, n):
+    grille = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            if rand() < p:
+                grille[i][j] = 1.
+    return grille
