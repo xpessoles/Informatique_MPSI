1648722fe12f169afe41f02eef14ad8a67d0e030
emiliendurif <emilien.durif@gmail.com>
2019-12-03 20:13:56 +0100

    modif cours 8

--- site_info_mpsi/exoCH5.py
+++ site_info_mpsi/exoCH5.py
@@ -3,7 +3,12 @@ M=[[4,7,9],[1,2,6],[3,3,8],[4,1,5]]
 
 def maxi_double(M):
     nligne,ncol=len(M),len(M[0])
-    for i in
+    max=M[0][0]
+    for i in range(nligne):
+        for j in range(ncol):
+            if M[i][j]>max:
+                max=M[i][j]
+    return max
 
 def maxi_double2(M):
     nligne,ncol=np.array(M).shape
