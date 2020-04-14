f8b76cc3f3f263e3c3f909df93541d8ae5f02e2e
emiliendurif <emilien.durif@gmail.com>
2020-04-14 23:52:01 +0200

    i

--- exos/systemes/pont_de_wheastone/pont_de_Wheatstone.py
+++ exos/systemes/pont_de_wheastone/pont_de_Wheatstone.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 24 06:38:15 2014
-
-@author: ericguichet
-"""
-
 import numpy as np
 
 # donnees
@@ -14,4 +7,4 @@ E=10
 A=np.array([[1,-1,0,0,-1],[0,0,1,-1,-1],[R1,R2,0,0,0],[0,0,R3,R4,0],[R1,0,0,-R4,R]])
 B=np.array([[0],[0],[E],[E],[0]])
 X=np.linalg.solve(A,B)
-print X[4,0]
+print(X[4,0])
