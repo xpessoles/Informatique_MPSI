39115614029d2f14800263bce54f3d7f8894bf8f
emiliendurif <emilien.durif@gmail.com>
2020-09-21 15:13:20 +0200

    maj cours 3

---
+++ site_info_mpsi/test_conversion.py
@@ -0,0 +1,12 @@
+def conversion(h,m,s):
+    """Convertit blablabla"""
+    t=3600*h+60*m+s
+    return t
+
+def conversion2(h,m,s):
+    """Convertit blablabla"""
+    t=3600*h+60*m+s
+    print(t)
+
+a=conversion(2,15,39)
+b=conversion2(2,15,39)
